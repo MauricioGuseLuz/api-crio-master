f323cc026cbbe92cbbf56f9c2fd08a37600020ed
Mauricio <soldadorasta23@gmail.com>
2024-09-19 21:48:45 -0300

    Inicio da autenticação

---
+++ src/main/java/com/crio/api/config/AutenticacaoJWT.java
@@ -0,0 +1,28 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoJWT {
+    private String secretKey = "secret";
+
+    public String createToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + 3600000); // 1h validade
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
